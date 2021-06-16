4799196c32ef0b56bc7994f8fbfab30f15e774e3
FullStackEngN <fullstackeng@outlook.com>
2021-06-16 10:54:25 +0800

    Update unicode decode

--- gzip_decompress.ps1
+++ gzip_decompress.ps1
@@ -1,15 +1,14 @@
 $gzipHex = Get-Content C:\Temp\tmp.txt
-$bytes = New-Object -TypeName byte[] -ArgumentList($gzipHex.Length / 2)
+$bytes = New-Object -TypeName byte[] -ArgumentList ($gzipHex.Length / 2)
 for ($i = 0; $i -lt $gzipHex.Length; $i += 2) {
    $bytes[$i / 2] = [Convert]::ToByte($gzipHex.Substring($i, 2), 16)
 }
-
 $ms = New-Object IO.MemoryStream(, $bytes)
 $gzs = New-Object IO.Compression.GzipStream($ms, [IO.Compression.CompressionMode]::Decompress)
 $sr = New-Object IO.StreamReader($gzs, [Text.Encoding]::UTF8)
 $decoded = $sr.ReadToEnd();
 
-[regex]::UnEscape($decoded)
+$decoded = [regex]::UnEscape($decoded)
 
 Set-Content C:\temp\tmp1.txt -Value $decoded
 
